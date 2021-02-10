5d26ab7afd10ad2f9acc71cc63fed911eb7538d6
Spencer Baynton <spencertbaynton@gmail.com>
2021-02-09 21:11:32 -0500

    Player movement

--- scenes/main.js
+++ scenes/main.js
@@ -1,7 +1,148 @@
+let cursors;
 let hazards;
 let player;
+let playerMoveX;
 let score;
 let scoreText;
+
+class Main extends Phaser.Scene {
+
+  constructor() {
+
+    super({ key: 'main' });
+
+    playerMoveX = 4;
+    score = 0;
+  }
+
+  create() {
+
+    cursors = this.input.keyboard.createCursorKeys();
+
+    scoreText = this.add.text(0, 16, 'Score: 0', {
+      align: 'center',
+      fixedWidth: this.game.config.width,
+      fontSize: '15px'
+    });
+
+    scoreText.setDepth(1);
+
+    // Hazards
+
+    hazards = this.physics.add.group();
+
+    this.time.addEvent({
+      callback: spawnHazard,
+      delay: 500,
+      loop: true
+    });
+
+    // Dumpster
+
+    this.anims.create({
+      key: 'burn',
+      frames: this.anims.generateFrameNumbers('dumpster'),
+      frameRate: 5,
+      repeat: -1
+    });
+
+    // Player
+
+    player = this.physics.add.sprite(
+      this.game.config.width / 2,
+      this.game.config.height - 32 // frameHeight
+    );
+
+    this.anims.create({
+      key: 'drive',
+      frames: this.anims.generateFrameNumbers('player', {
+        frames: [ 0, 1, 2 ]
+      }),
+      frameRate: 5,
+      repeat: -1
+    });
+
+    player.play('drive');
+    player.setCollideWorldBounds(true);
+    player.setDepth(1);
+
+    // Physics
+
+    this.physics.add.collider(player, hazards);
+
+    this.physics.add.overlap(player, hazards, () => {
+
+      this.add.text(0, 50, 'Game Over', {
+        align: 'center',
+        color: 'white',
+        fixedWidth: this.game.config.width,
+        fontFamily: 'Arial',
+        fontSize: 36
+      });
+
+      this.scene.pause();
+    });
+  }
+
+  preload() {
+
+    this.load.path = '/assets/';
+
+    this.load.image('bus');
+    this.load.image('car-1');
+    this.load.image('car-2');
+    this.load.image('car-3');
+    this.load.image('line');
+    this.load.image('oil');
+    this.load.image('pothole');
+
+    this.load.spritesheet({
+      key: 'dumpster',
+      frameConfig: {
+        frameWidth: 32,
+        frameHeight: 32
+      }
+    });
+
+    this.load.spritesheet({
+      key: 'player',
+      frameConfig: { frameWidth: 32 }
+    });
+
+    this.load.spritesheet({
+      key: 'semi',
+      frameConfig: {
+        frameWidth: 32,
+        frameHeight: 64
+      }
+    });
+  }
+
+  update() {
+
+    if (cursors.left.isDown) {
+      // Move player left
+      player.x -= playerMoveX;
+    } else if (cursors.right.isDown) {
+      // Move player right
+      player.x += playerMoveX;
+    }
+
+    Phaser.Actions.IncY(hazards.getChildren(), 3);
+
+    hazards.children.iterate((hazard) => {
+      // Is the hazard active and off the bottom of the screen?
+      if (
+        hazard.active
+        && hazard.y > this.game.config.height + hazard.height / 2
+      ) {
+        hazard.stop();
+        hazards.killAndHide(hazard);
+        scoreText.setText(`Score: ${++score}`);
+      }
+    });
+  }
+}
 
 /**
  * Spawns a hazard in a random lane.
@@ -38,136 +179,6 @@ function spawnHazard() {
   // HACK
   if (key === 'dumpster') {
     hazard.play('burn');
-  }
-}
-
-class Main extends Phaser.Scene {
-
-  constructor() {
-
-    super({ key: 'main' });
-
-    score = 0;
-  }
-
-  create() {
-
-    scoreText = this.add.text(0, 16, 'Score: 0', {
-      align: 'center',
-      fixedWidth: this.game.config.width,
-      fontSize: '15px'
-    });
-
-    scoreText.setDepth(1);
-
-    // Hazards
-
-    hazards = this.physics.add.group();
-
-    this.time.addEvent({
-      callback: spawnHazard,
-      delay: 1000,
-      loop: true
-    });
-
-    // Dumpster
-
-    this.anims.create({
-      key: 'burn',
-      frames: this.anims.generateFrameNumbers('dumpster'),
-      frameRate: 4,
-      repeat: -1
-    });
-
-    // Player
-
-    this.anims.create({
-      key: 'drive',
-      frames: this.anims.generateFrameNumbers('player', { frames: [ 0, 1, 2 ] }),
-      frameRate: 4,
-      repeat: -1
-    });
-
-    player = this.physics.add.sprite(
-      this.game.config.width / 2,
-      this.game.config.height - 32 // frameHeight
-    );
-
-    player.play('drive');
-    player.setCollideWorldBounds(true);
-    player.setDepth(1);
-
-    this.physics.add.collider(player, hazards);
-
-    this.physics.add.overlap(player, hazards, () => {
-
-      this.add.text(0, 50, 'Game Over', {
-        align: 'center',
-        color: 'white',
-        fixedWidth: this.game.config.width,
-        fontFamily: 'Arial',
-        fontSize: 36
-      });
-
-      this.scene.pause();
-    });
-  }
-
-  preload() {
-
-    this.load.path = '/assets/';
-
-    this.load.image('car-1');
-    this.load.image('car-2');
-    this.load.image('car-3');
-    this.load.image('oil');
-    this.load.image('pothole');
-
-    this.load.spritesheet({
-      key: 'bus',
-      frameConfig: {
-        frameWidth: 32,
-        frameHeight: 64
-      }
-    });
-
-    this.load.spritesheet({
-      key: 'dumpster',
-      frameConfig: {
-        frameWidth: 32,
-        frameHeight: 32
-      }
-    });
-
-    this.load.spritesheet({
-      key: 'player',
-      frameConfig: { frameWidth: 32 }
-    });
-
-    this.load.spritesheet({
-      key: 'semi',
-      frameConfig: {
-        frameWidth: 32,
-        frameHeight: 64
-      }
-    });
-  }
-
-  update() {
-
-    Phaser.Actions.IncY(hazards.getChildren(), 3);
-
-    hazards.children.iterate((hazard) => {
-      // Is the hazard active and off the bottom of the screen?
-      if (
-        hazard.active
-        && hazard.y > this.game.config.height + hazard.height / 2
-      ) {
-        hazard.stop();
-        hazards.killAndHide(hazard);
-        scoreText.setText(`Score: ${++score}`);
-      }
-    });
   }
 }
 
